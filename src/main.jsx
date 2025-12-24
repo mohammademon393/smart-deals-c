ef7f8e0b784453345eebc1862daa27496c1763e1
mohammademon393 <mdemon357ep@gmail.com>
2025-12-24 23:58:39 +0600

    emplement folder structure and create five jsx components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from './layout/RootLayout';
+import Home from './pages/Home';
+import AllProducts from './pages/AllProducts';
 
-createRoot(document.getElementById('root')).render(
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout></RootLayout>,
+    children:[
+      {
+        index:true,
+        Component: Home,
+      },
+      {
+        path: '/allProducts',
+        Component: AllProducts,
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />,
+  </StrictMode>
+);
